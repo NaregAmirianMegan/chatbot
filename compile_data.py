5d04b1dddfcee879cd68a3e79385229f4392ed66
Nareg Megan <nareg@Naregs-MacBook-Pro.local>
2018-12-25 00:58:46 -0600

    implement multi-file sampling

--- compile_data.py
+++ compile_data.py
@@ -4,10 +4,10 @@ import sqlite3
 def compile_data(test_size, train_size, database):
     assert train_size > test_size, "train_size must be larger than test_size"
 
-    test_text = open("sample-data/{}.test.text".format(database), "a")
-    test_reply = open("sample-data/{}.test.reply".format(database), "a")
-    train_text = open("sample-data/{}.train.text".format(database), "a")
-    train_reply = open("sample-data/{}.train.reply".format(database), "a")
+    test_text = open("compiled_data/test.text", "a")
+    test_reply = open("compiled_data/test.reply", "a")
+    train_text = open("compiled_data/train.text", "a")
+    train_reply = open("compiled_data/train.reply", "a")
 
     connection = sqlite3.connect('{}.db'.format('data/'+database))
     c = connection.cursor()
@@ -15,13 +15,22 @@ def compile_data(test_size, train_size, database):
     data = c.fetchall()
 
     for row in data:
-        if test_size:
-            test_text.write(row[0]+'\n')
-            test_reply.write(row[1]+'\n')
-            test_size -= 1
-        else:
-            train_text.write(row[0]+'\n')
-            train_reply.write(row[1]+'\n')
+        text = row[0]
+        reply = row[1] 
+        if not (text == None or reply == None):
+            if test_size:
+                test_text.write(text+"\n")
+                test_reply.write(reply+"\n")
+                test_size -= 1
+            else:
+                train_text.write(text+"\n")
+                train_reply.write(reply+"\n")
+
+def create_compilation(file_list, test_size, train_size):
+    div = len(file_list)
+    for file in file_list:
+        compile_data(int(test_size/div), int(train_size/div), file)
 
 if __name__ == '__main__':
-    compile_data(100, 1000, 'RC_2018-01')
+    files = ['RC_2015-05', 'RC_2016-01', 'RC_2017-01', 'RC_2017-02', 'RC_2017-12', 'RC_2018-01', 'RC_2018-02']
+    create_compilation(files, 21000, 21000000)
