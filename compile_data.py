0ba2bcf8acda9b58ca3118354465c7b306456fbc
Nareg Megan <nareg@Naregs-MacBook-Pro.local>
2018-12-24 23:51:26 -0600

    add sample data folder

--- compile_data.py
+++ compile_data.py
@@ -4,10 +4,10 @@ import sqlite3
 def compile_data(test_size, train_size, database):
     assert train_size > test_size, "train_size must be larger than test_size"
 
-    test_text = open("compiled_data/{}.test.text".format(database), "a")
-    test_reply = open("compiled_data/{}.test.reply".format(database), "a")
-    train_text = open("compiled_data/{}.train.text".format(database), "a")
-    train_reply = open("compiled_data/{}.train.reply".format(database), "a")
+    test_text = open("sample-data/{}.test.text".format(database), "a")
+    test_reply = open("sample-data/{}.test.reply".format(database), "a")
+    train_text = open("sample-data/{}.train.text".format(database), "a")
+    train_reply = open("sample-data/{}.train.reply".format(database), "a")
 
     connection = sqlite3.connect('{}.db'.format('data/'+database))
     c = connection.cursor()
@@ -24,4 +24,4 @@ def compile_data(test_size, train_size, database):
             train_reply.write(row[1]+'\n')
 
 if __name__ == '__main__':
-    compile_data(10, 100, 'RC_2018-01')
+    compile_data(100, 1000, 'RC_2018-01')
